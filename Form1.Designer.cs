74ce9d43021626036003d14fb04a2ca93211f262
Lucky <Lucky@KOMP>
2025-03-09 23:35:22 +0600

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        private void InitializeComponent()
+        {
+            this.txtSourcePath = new System.Windows.Forms.TextBox();
+            this.txtDestinationPath = new System.Windows.Forms.TextBox();
+            this.btnSelectSource = new System.Windows.Forms.Button();
+            this.btnSelectDestination = new System.Windows.Forms.Button();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(584, 5);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 216);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(584, 5);
+            this.panel2.TabIndex = 8;
+            // 
+            // txtSourcePath
+            // 
+            this.txtSourcePath.BackColor = System.Drawing.Color.White;
+            this.txtSourcePath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSourcePath.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular);
+            this.txtSourcePath.Location = new System.Drawing.Point(22, 39);
+            this.txtSourcePath.Name = "txtSourcePath";
+            this.txtSourcePath.ReadOnly = true;
+            this.txtSourcePath.Size = new System.Drawing.Size(459, 23);
+            this.txtSourcePath.TabIndex = 0;
+            // 
+            // txtDestinationPath
+            // 
+            this.txtDestinationPath.BackColor = System.Drawing.Color.White;
+            this.txtDestinationPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDestinationPath.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular);
+            this.txtDestinationPath.Location = new System.Drawing.Point(22, 100);
+            this.txtDestinationPath.Name = "txtDestinationPath";
+            this.txtDestinationPath.ReadOnly = true;
+            this.txtDestinationPath.Size = new System.Drawing.Size(459, 23);
+            this.txtDestinationPath.TabIndex = 1;
+            // 
+            // btnSelectSource
+            // 
+            this.btnSelectSource.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.btnSelectSource.FlatAppearance.BorderSize = 0;
+            this.btnSelectSource.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelectSource.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular);
+            this.btnSelectSource.ForeColor = System.Drawing.Color.White;
+            this.btnSelectSource.Location = new System.Drawing.Point(487, 39);
+            this.btnSelectSource.Name = "btnSelectSource";
+            this.btnSelectSource.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectSource.TabIndex = 2;
+            this.btnSelectSource.Text = "Обзор...";
+            this.btnSelectSource.UseVisualStyleBackColor = false;
+            // 
+            // btnSelectDestination
+            // 
+            this.btnSelectDestination.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.btnSelectDestination.FlatAppearance.BorderSize = 0;
+            this.btnSelectDestination.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelectDestination.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular);
+            this.btnSelectDestination.ForeColor = System.Drawing.Color.White;
+            this.btnSelectDestination.Location = new System.Drawing.Point(487, 100);
+            this.btnSelectDestination.Name = "btnSelectDestination";
+            this.btnSelectDestination.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectDestination.TabIndex = 3;
+            this.btnSelectDestination.Text = "Обзор...";
+            this.btnSelectDestination.UseVisualStyleBackColor = false;
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.btnBackup.FlatAppearance.BorderSize = 0;
+            this.btnBackup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBackup.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnBackup.ForeColor = System.Drawing.Color.White;
+            this.btnBackup.Location = new System.Drawing.Point(22, 144);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(540, 50);
+            this.btnBackup.TabIndex = 4;
+            this.btnBackup.Text = "БЭКАП";
+            this.btnBackup.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(19, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Исходная папка:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(19, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Папка для бэкапа:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(584, 221);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBackup);
+            this.Controls.Add(this.btnSelectDestination);
+            this.Controls.Add(this.btnSelectSource);
+            this.Controls.Add(this.txtDestinationPath);
+            this.Controls.Add(this.txtSourcePath);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Backup Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox txtSourcePath;
+        private System.Windows.Forms.TextBox txtDestinationPath;
+        private System.Windows.Forms.Button btnSelectSource;
+        private System.Windows.Forms.Button btnSelectDestination;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private NotifyIcon trayIcon;
+        private ContextMenuStrip trayMenu;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTrayIcon();
+            this.Text = $"Бэкап лайт v{Assembly.GetExecutingAssembly().GetName().Version}";
+
+            // Замените MyIcon на имя вашей иконки в ресурсах
+            using (var stream = new MemoryStream(Properties.Resources.MyIcon))
+            {
+                this.Icon = new Icon(stream, new Size(16, 16));
+            }
+
+            // Загрузка сохраненных путей
+            LoadPaths();
+
+            // Существующие обработчики
+            btnSelectSource.Click += BtnSelectSource_Click;
+            btnSelectDestination.Click += BtnSelectDestination_Click;
+            btnBackup.Click += BtnBackup_Click;
+
+            // Добавляем эффекты при наведении для кнопок
+            ConfigureButtonHoverEffects(btnSelectSource);
+            ConfigureButtonHoverEffects(btnSelectDestination);
+            ConfigureButtonHoverEffects(btnBackup);
+
+            // Обработчик сворачивания окна
+            this.Resize += MainForm_Resize;
+        }
+
+        private void InitializeTrayIcon()
+        {
+            trayMenu = new ContextMenuStrip();
+            trayMenu.Items.Add("Развернуть", null, OnTrayRestore);
+            trayMenu.Items.Add("Бэкап", null, (s, e) => BtnBackup_Click(s, e));
+            trayMenu.Items.Add("-"); // Разделитель
+            trayMenu.Items.Add("О программе", null, OnAbout);
+            trayMenu.Items.Add("Выход", null, OnTrayExit);
+
+            trayIcon = new NotifyIcon
+            {
+                Text = "Бэкап лайт",
+                Icon = new Icon(new MemoryStream(Properties.Resources.MyIcon)), // Используем иконку из ресурсов
+                ContextMenuStrip = trayMenu,
+                Visible = false
+            };
+
+            // Убираем обработчик одиночного клика, оставляем только двойной
+            trayIcon.MouseClick += (s, e) =>
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    OnTrayRestore(s, e);
+                }
+            };
+        }
+
+        private void OnAbout(object sender, EventArgs e)
+        {
+            MessageBox.Show("© 2025 Бэкап лайт. Все права защищены. Разработчик Lucky", "О программе", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                trayIcon.Visible = true;
+                trayIcon.BalloonTipTitle = "Бэкап лайт";
+                trayIcon.BalloonTipText = "Приложение свернуто в трей";
+                trayIcon.BalloonTipIcon = ToolTipIcon.Info;
+                trayIcon.ShowBalloonTip(3000); // Увеличиваем время отображения до 3000 миллисекунд
+            }
+        }
+
+        private void OnTrayRestore(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            trayIcon.Visible = false;
+        }
+
+        private void OnTrayExit(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (trayIcon != null)
+            {
+                trayIcon.Dispose();
+            }
+            if (trayMenu != null)
+            {
+                trayMenu.Dispose();
+            }
+            base.OnFormClosing(e);
+        }
+
+        private void ConfigureButtonHoverEffects(Button button)
+        {
+            button.MouseEnter += (s, e) =>
+            {
+                button.BackColor = System.Drawing.Color.FromArgb(52, 152, 219);
+            };
+            button.MouseLeave += (s, e) =>
+            {
+                button.BackColor = System.Drawing.Color.FromArgb(41, 128, 185);
+            };
+        }
+
+        private void BtnSelectSource_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Логика для обработки выбранной папки
+                    string selectedPath = folderBrowserDialog.SelectedPath;
+                    // Например, сохранить путь в текстовое поле
+                    txtSourcePath.Text = selectedPath;
+                    SavePaths();
+                }
+            }
+        }
+
+        private void BtnSelectDestination_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Логика для обработки выбранной папки
+                    string selectedPath = folderBrowserDialog.SelectedPath;
+                    // Например, сохранить путь в текстовое поле
+                    txtDestinationPath.Text = selectedPath;
+                    SavePaths();
+                }
+            }
+        }
+
+        private void BtnBackup_Click(object sender, EventArgs e)
+        {
+            string sourcePath = txtSourcePath.Text;
+            string destinationPath = txtDestinationPath.Text;
+
+            if (Directory.Exists(sourcePath) && Directory.Exists(destinationPath))
+            {
+                try
+                {
+                    string backupFolderName = Path.GetFileName(sourcePath) + "_Backup_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+                    string backupFolderPath = Path.Combine(destinationPath, backupFolderName);
+
+                    Directory.CreateDirectory(backupFolderPath);
+
+                    foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+                    {
+                        Directory.CreateDirectory(dirPath.Replace(sourcePath, backupFolderPath));
+                    }
+
+                    foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+                    {
+                        File.Copy(newPath, newPath.Replace(sourcePath, backupFolderPath), true);
+                    }
+
+                    ShowAutoClosingMessageBox("Резервное копирование успешно завершено!", "Успех", 500);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Произошла ошибка во время резервного копирования: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите допустимые пути для исходной и конечной папок.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void ShowAutoClosingMessageBox(string message, string title, int timeout)
+        {
+            var timer = new Timer();
+            timer.Interval = timeout;
+            timer.Tick += (s, e) =>
+            {
+                var msgBox = (Form)((Timer)s).Tag;
+                msgBox.Close();
+                timer.Stop();
+                timer.Dispose();
+            };
+            timer.Start();
+
+            var msgForm = new Form()
+            {
+                Text = title,
+                StartPosition = FormStartPosition.CenterScreen,
+                Size = new System.Drawing.Size(300, 150),
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                BackColor = System.Drawing.Color.FromArgb(41, 128, 185),
+                ForeColor = System.Drawing.Color.White
+            };
+            var label = new Label()
+            {
+                Text = message,
+                Dock = DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Arial", 12, System.Drawing.FontStyle.Bold),
+                ForeColor = System.Drawing.Color.White
+            };
+            msgForm.Controls.Add(label);
+            timer.Tag = msgForm;
+            msgForm.ShowDialog();
+        }
+
+        private void SavePaths()
+        {
+            Properties.Settings.Default["SourcePath"] = txtSourcePath.Text;
+            Properties.Settings.Default["DestinationPath"] = txtDestinationPath.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void LoadPaths()
+        {
+            txtSourcePath.Text = Properties.Settings.Default["SourcePath"]?.ToString();
+            txtDestinationPath.Text = Properties.Settings.Default["DestinationPath"]?.ToString();
+        }
+    }
+}
